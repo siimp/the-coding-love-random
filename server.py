e46d1332332df1f0df25aca29dece8510eb5e9de
siim <siimpuustusmaa@gmail.com>
2020-10-09 08:45:05 +0300

    code cleanup

--- server.py
+++ server.py
@@ -1,31 +1,38 @@
-import requests
 import http.server
 import socketserver
-from bs4 import BeautifulSoup
+
+import bs4
+import requests
 
 THE_CODING_LOVE_RANDOM_URL = 'https://thecodinglove.com/random'
 PORT = 8087
 
-def get_simplified_html():
-    response = requests.get(THE_CODING_LOVE_RANDOM_URL)
-    soup = BeautifulSoup(response.content, features='html.parser')
-    article = soup.find('article')
-    heading = article.find('h1')
-    content = article.find('div', class_='blog-post-content')
 
+def get_template(heading, content):
     return f'''
         <!DOCTYPE html>
         <html lang="">
         <head>
             <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
             <title>the conding love random</title>
         </head>
         <body>
-        <h1>{heading}</h1>
+        {heading}
         {content}
         </body>
         </html>
     '''
+
+
+def get_simplified_html():
+    response = requests.get(THE_CODING_LOVE_RANDOM_URL)
+    soup = bs4.BeautifulSoup(response.content, features='html.parser')
+    article = soup.find('article')
+    heading = article.find('h1')
+    content = article.find('div', class_='blog-post-content')
+    return get_template(heading, content)
+
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -33,11 +40,11 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(bytes(get_simplified_html(), 'utf8'))
-        return
 
-with socketserver.TCPServer(('', PORT), MyHttpRequestHandler) as httpd:
-    print('serving at port', PORT)
-    print('content url is', THE_CODING_LOVE_RANDOM_URL)
-    httpd.serve_forever()
+
+server = socketserver.TCPServer(('', PORT), MyHttpRequestHandler)
+print(f'content url is: {THE_CODING_LOVE_RANDOM_URL}')
+print(f'serving at http://localhost:{PORT}')
+server.serve_forever()
 
 
